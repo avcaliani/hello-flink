85c44385c64571241b0b0ff568168002ca14dd3d
Anthony Caliani <avcaliani.it@gmail.com>
2025-05-01 20:28:03 -0300

    fix: Fixing imports + doc updates.

--- src/main/java/br/avcaliani/hello_flink/models/User.java
+++ src/main/java/br/avcaliani/hello_flink/models/User.java
@@ -1,9 +1,7 @@
 package br.avcaliani.hello_flink.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.util.List;
 
 @Data
 public class User {
@@ -14,12 +12,13 @@ public class User {
     @JsonProperty("user_name")
     private String name;
 
-    /** <b>Developer Note</b>
+    /**
+     * <b>Developer Note</b>
      * </br>
      * This field should be created dynamically by a Map, since it's not in the CSV file.</br>
      * I believe the best option would be creating a User, to represent the CSV schema,
      * and a User DTO that will have the extra fields.
-     *</br></br>
+     * </br></br>
      * However, since this is just a PoC I'm not going to create the DTO.
      * </br></br>
      * 💡 Do you have a different opinion? Isn't it the best approach? Let me know?
