ed7a103160836affb7c25b8c70adf801520fd409
Anthony Caliani <avcaliani.it@gmail.com>
2025-07-17 19:02:15 -0300

    refactor: Creating Kafka wrapper.

--- src/main/java/br/avcaliani/hello_flink/infra/Kafka.java
+++ src/main/java/br/avcaliani/hello_flink/infra/Kafka.java
@@ -1,4 +1,51 @@
 package br.avcaliani.hello_flink.infra;
 
+import br.avcaliani.hello_flink.infra.serializers.KafkaDeserializer;
+import br.avcaliani.hello_flink.infra.serializers.KafkaMessage;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
 public class Kafka {
+
+    private final StreamExecutionEnvironment env;
+    private final String brokers;
+
+    public Kafka(StreamExecutionEnvironment env, String brokers) {
+        this.env = env;
+        this.brokers = brokers;
+    }
+
+    /**
+     * Create a Data Stream based on Kafka Source.
+     *
+     * @param topic   Kafka Topic.
+     * @param groupId Kafka Consumer - Group ID.
+     * @param clazz   POJO Class Kafka Message (also the 'T')
+     * @return Data Stream.
+     */
+    public <T extends KafkaMessage> DataStream<T> read(String topic, String groupId, Class<T> clazz) {
+
+        KafkaSource<T> source = KafkaSource.<T>builder()
+                .setBootstrapServers(this.brokers)
+                .setTopics(topic)
+                .setGroupId(groupId)
+                .setStartingOffsets(OffsetsInitializer.earliest())
+                .setDeserializer(new KafkaDeserializer<>(clazz))
+                .build();
+
+        return this.env.fromSource(source, WatermarkStrategy.noWatermarks(), sourceName(topic));
+    }
+
+    /**
+     * The source name is always "kafka-source--topic-name".
+     *
+     * @param topic Kafka Topic.
+     * @return Flink source name.
+     */
+    private String sourceName(String topic) {
+        return "kafka-source--" + topic.replace('_', '-').toLowerCase();
+    }
 }
