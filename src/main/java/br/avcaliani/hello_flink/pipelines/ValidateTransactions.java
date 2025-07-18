29056a7f9b42d0b217bcbf362e5569c474548ed2
Anthony Caliani <avcaliani.it@gmail.com>
2025-07-18 01:13:39 -0300

    refactor: Adding sink method to Kafka wrapper.

--- src/main/java/br/avcaliani/hello_flink/pipelines/ValidateTransactions.java
+++ src/main/java/br/avcaliani/hello_flink/pipelines/ValidateTransactions.java
@@ -3,18 +3,15 @@ package br.avcaliani.hello_flink.pipelines;
 import br.avcaliani.hello_flink.cli.Args;
 import br.avcaliani.hello_flink.infra.CSV;
 import br.avcaliani.hello_flink.infra.Kafka;
-import br.avcaliani.hello_flink.infra.serializers.KafkaSerializer;
 import br.avcaliani.hello_flink.models.in.Transaction;
 import br.avcaliani.hello_flink.models.in.User;
 import br.avcaliani.hello_flink.models.out.DTOTransaction;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.connector.base.DeliveryGuarantee;
-import org.apache.flink.connector.kafka.sink.KafkaSink;
-import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
 import org.apache.flink.util.Collector;
+
+import static org.apache.flink.connector.base.DeliveryGuarantee.EXACTLY_ONCE;
 
 
 /**
@@ -26,9 +23,6 @@ import org.apache.flink.util.Collector;
  * </a>
  */
 public class ValidateTransactions extends Pipeline {
-
-    private static final String KAFKA_OUT_TOPIC = "DONU_EXECUTE_TRANSACTION_V1";
-    private static final String KAFKA_DEAD_LETTER = "DLQ_DONU_TRANSACTIONS_V1";
 
     @Override
     public Pipeline run(Args args) throws Exception {
@@ -46,34 +40,46 @@ public class ValidateTransactions extends Pipeline {
         );
 
         var richTxn = joinData(transactions, users)
-                .map(isTxnValid());
+                .map(this::isTxnValid);
 
-        // https://nightlies.apache.org/flink/flink-docs-master/docs/connectors/datastream/kafka/
-        var dlqSink = KafkaSink.<DTOTransaction>builder()
-                .setBootstrapServers(args.getKafkaBrokers())
-                .setRecordSerializer(new KafkaSerializer<>(KAFKA_DEAD_LETTER))
-                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-                .build();
-
-        richTxn.filter(DTOTransaction::isInvalid).sinkTo(dlqSink);
-
-        // Kafka brokers by default have transaction.max.timeout.ms set to 15 minutes.
-        // This property will not allow to set transaction timeouts for the producers larger than its value.
-        // FlinkKafkaProducer by default sets the transaction.timeout.ms property in producer config to 1 hour,
-        // thus transaction.max.timeout.ms should be increased before using the Semantic.EXACTLY_ONCE mode.
-        // Ref: https://nightlies.apache.org/flink/flink-docs-release-1.13/docs/connectors/datastream/kafka/
-        var validSink = KafkaSink.<DTOTransaction>builder()
-                .setBootstrapServers(args.getKafkaBrokers())
-                .setRecordSerializer(new KafkaSerializer<>(KAFKA_OUT_TOPIC))
-                // https://flink.apache.org/2018/02/28/an-overview-of-end-to-end-exactly-once-processing-in-apache-flink-with-apache-kafka-too/
-                .setDeliveryGuarantee(DeliveryGuarantee.EXACTLY_ONCE)
-                .setProperty("transaction.timeout.ms", "900000")
-                .setTransactionalIdPrefix("flink-valid-txn") // Mandatory when using Exactly Once
-                .build();
+        var validSink = kafka.<DTOTransaction>sink(
+                "DONU_EXECUTE_TRANSACTION_V1", /* Topic */
+                EXACTLY_ONCE /* Delivery Guarantee */
+        );
         richTxn.filter(DTOTransaction::isValid).sinkTo(validSink);
+
+        var deadLetterSink = kafka.<DTOTransaction>sink(
+                "DLQ_DONU_TRANSACTIONS_V1" /* Topic */
+        );
+        richTxn.filter(DTOTransaction::isInvalid).sinkTo(deadLetterSink);
 
         env.execute("hello-flink--validate-transactions");
         return this;
+    }
+
+    /**
+     * Check and update the traction "isValid" field.
+     *
+     * @return Updated Transaction.
+     */
+    private DTOTransaction isTxnValid(DTOTransaction txn) {
+
+        var isMissingUser = txn.getSender() == null || txn.getReceiver() == null;
+        var isSameUser = txn.getSender() == txn.getReceiver();
+        var isValid = !isMissingUser && !isSameUser;
+
+        txn.setIsValid(isValid);
+        if (isValid)
+            return txn;
+
+        var errors = txn.getErrors();
+        if (isMissingUser)
+            errors.add("missing user(s) for transaction (from:" + txn.getFrom() + " -> to:" + txn.getTo() + ").");
+
+        if (isSameUser)
+            errors.add("transaction have the same user as sender and receiver.");
+
+        return txn;
     }
 
     /**
@@ -85,38 +91,10 @@ public class ValidateTransactions extends Pipeline {
      */
     private DataStream<DTOTransaction> joinData(DataStream<Transaction> transactions, DataStream<User> users) {
         var userStateDescriptor = new MapStateDescriptor<>("userBroadcastState", String.class, User.class);
-        BroadcastStream<User> broadcastUserStream = users.broadcast(userStateDescriptor);
-        // TODO: Explain why I'm using connect and not a window join.
+        var broadcastUserStream = users.broadcast(userStateDescriptor);
         return transactions
                 .connect(broadcastUserStream)
                 .process(new EnrichTxnFunction(userStateDescriptor));
-    }
-
-    /**
-     * Check and update the traction "isValid" field.
-     *
-     * @return Updated Transaction.
-     */
-    private MapFunction<DTOTransaction, DTOTransaction> isTxnValid() {
-        return txn -> {
-
-            var isMissingUser = txn.getSender() == null || txn.getReceiver() == null;
-            var isSameUser = txn.getSender() == txn.getReceiver();
-            var isValid = !isMissingUser && !isSameUser;
-
-            txn.setIsValid(isValid);
-            if (isValid)
-                return txn;
-
-            var errors = txn.getErrors();
-            if (isMissingUser)
-                errors.add("missing user(s) for transaction (from:" + txn.getFrom() + " -> to:" + txn.getTo() + ").");
-
-            if (isSameUser)
-                errors.add("transaction have the same user as sender and receiver.");
-
-            return txn;
-        };
     }
 
     /**
